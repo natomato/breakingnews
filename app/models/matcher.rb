97bda05cc6a16737d8c4b1cdb749897081a60f7a
Nate Tate <nate91711@gmail.com>
2013-12-21 23:16:45 -0800

    added matcher model

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -9,4 +9,28 @@ class Matcher
     false
   end
 
+  def get_lead_paragraph(link)
+  doc = Nokogiri::HTML(open(link))
+  candidates = []
+
+  matchers = [ 'p',
+               '#article-body p',
+               '#slide-content p',
+             ]
+ 
+  matchers.each do |matcher, url_parts|
+    next if doc.css(matcher).empty?
+    paragraph = doc.css(matcher)[0].text
+
+    if link.split('/')[3] == "video"
+      paragraph.concat( " Follow link to watch the full video." )
+    end
+
+    candidates << paragraph
+  end
+
+  pick_best_paragraph(candidates)
+
+  end
+
 end
